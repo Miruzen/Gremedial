2d46d4e6207bf496fca05f9f2a3f74cbdb3e0c3d
Miruzen <nandagouwputra@gmail.com>
2023-11-28 23:30:30 +0700

    Text

--- Detail.js
+++ Detail.js
@@ -2,16 +2,14 @@
 document.querySelectorAll('.btnDetail').forEach(item => {
     item.addEventListener('click', (e) => {
         let parent = e.target.parentNode.parentNode;
-  
+
         let gambar = parent.querySelector('.card-img-top').src;
         let harga = parent.querySelector('.harga').innerHTML;
         let judul = parent.querySelector('.card-text').innerHTML;
         let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML : '<i>tidak ada informasi yang tersedia</i>';
-  
-  
         let tombolModal = document.querySelector('.btnModal');
+
         tombolModal.click();
-  
         document.querySelector('.modalTitle').innerHTML = judul;
         let image = document.createElement('img');
         image.src = gambar;
@@ -20,10 +18,9 @@ document.querySelectorAll('.btnDetail').forEach(item => {
         document.querySelector('.modalImage').appendChild(image);
         document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
         document.querySelector('.modalHarga').innerHTML = harga;
-  
+
         const nohp = '6287780992445';
         let pesan = `https://api.whatsapp.com/send?phone=${nohp}&text=Halo Bang, saya mau pesan produk ini ${gambar}`;
-  
         document.querySelector('.btnBeli').href = pesan;
     });
-  });
+});
